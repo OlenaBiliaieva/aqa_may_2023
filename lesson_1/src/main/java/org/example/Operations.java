f4bba6d6eaadd5deb4c7083777b67e58785aae60
Olena Biliaieva <Olena.Biliaieva@interbrew.net>
2023-05-23 20:02:52 +0300

    lesson_3

---
+++ lesson_1/src/main/java/org/example/Operations.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class Operations {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 12;
+
+        int sum = a + b;
+        System.out.println(sum);
+//        System.out.println(a + b);
+//        System.out.println(10 + 12);
+
+        int diff = a - b;
+        System.out.println(diff);
+
+        int mult = a * b;
+        System.out.println(mult);
+
+        float dev = (float) a / b;
+        System.out.println(dev);
+
+
+    }
+
+}
