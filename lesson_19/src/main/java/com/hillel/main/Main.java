0a127b53d2d284809c2553f218d35718b7a5e8af
Olena Biliaieva <Olena.Biliaieva@interbrew.net>
2023-07-27 21:00:42 +0300

    lesson_19

--- lesson_19/src/main/java/com/hillel/main/Main.java
+++ lesson_19/src/main/java/com/hillel/main/Main.java
@@ -4,10 +4,12 @@ import static com.hillel.main.ProjectLogger.logger;
 
 public class Main {
 
-    public static void main(final String... args)
-    {
-        logger.error("Debug Message Logged !!!");
-        logger.info("Info Message Logged !!!");
-        logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
+    public static void main(final String... args) {
+
+        String hello = "Hello this is log";
+        logger.info("String was added: {}", hello);
+//        logger.error("Debug Message Logged !!!");
+//        logger.info("Info Message Logged !!!");
+//        logger.error("Error Message Logged !!!", new NullPointerException("NullError"));
     }
 }
