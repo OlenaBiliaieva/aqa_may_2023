0a8f81e030242e232a315d3d07643d86dff20837
Olena Biliaieva <Olena.Biliaieva@interbrew.net>
2023-08-01 19:12:25 +0300

    lesson_19

--- lesson_19/src/main/java/com/hillel/lomb/pers/Person.java
+++ lesson_19/src/main/java/com/hillel/lomb/pers/Person.java
@@ -4,17 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 
 @Data
-@EqualsAndHashCode
 @AllArgsConstructor
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Person {
     private String name;
     private int age;
     List<String> product;
+
 
 }
